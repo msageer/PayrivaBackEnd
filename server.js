2e48ed208d8b2c546f0db5f05945be4b082272c8
Msageer <39546323+msageer@users.noreply.github.com>
2025-03-17 11:11:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
